e6b12ebcd6cf8d52d9b0eba961d59ee78d0ea54c
felpfsf <feferreira.sf@gmail.com>
2022-08-23 18:25:48 -0300

    feat: go back button color switch logic

--- day-8-theme-switcher/main.js
+++ day-8-theme-switcher/main.js
@@ -2,6 +2,9 @@ const toggle = document.querySelector('#toggle')
 toggle.addEventListener('click', () => {
   let element = document.body
   let title = document.querySelector('.title')
+  let goBack = document.querySelector('.goBack').childNodes
+
   element.classList.toggle('dark')
   title.classList.toggle('neon')
+  goBack.classList.toggle('dark')
 })
