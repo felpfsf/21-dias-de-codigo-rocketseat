c1ea0aa88195436b083a5de3ab19150682c895e8
felpfsf <feferreira.sf@gmail.com>
2022-08-24 16:39:01 -0300

    refac: logic and style remover

--- day-9-animated-progressbar/main.js
+++ day-9-animated-progressbar/main.js
@@ -1,20 +1,36 @@
 let btn = document.querySelector('#clickBtn')
+let msg = document.querySelector('.message')
+let meter = document.querySelector('.meter')
+
+meter.style.opacity = '0'
+
 btn.addEventListener('click', move)
 
 function move() {
   let barra = document.querySelector('#barra')
   let width = 0
+  meter.style.opacity = '1'
+
   let id = setInterval(frame, 48)
 
   function frame() {
+    msg.classList.remove('neon', 'skewed')
+    msg.innerHTML = 'Carregando...'
+
     if (width >= 100) {
+
       clearInterval(id)
-      btn.style.opacity = '1'
+      btn.removeAttribute('disabled', false)
+      msg.innerHTML = `dia 9 de #21diasdecodigo <br> Concluído !!!`
+      msg.classList.add('neon', 'skewed')
+      msg.style.opacity = '1'
+
     } else {
+      
       width++
       barra.style.width = width + '%'
       barra.innerHTML = width * 1 + '%'
-      btn.style.opacity = '0'
+      btn.setAttribute('disabled', true)
     }
   }
 }
