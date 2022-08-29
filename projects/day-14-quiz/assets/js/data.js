abb22d58c888b685cfe4e40de887fddbea24d8f1
felpfsf <feferreira.sf@gmail.com>
2022-08-29 16:58:38 -0300

    update: quiz logic

---
+++ projects/day-14-quiz/assets/js/data.js
@@ -0,0 +1,107 @@
+export const questions = [
+  {
+    question: 'Qual bicho transmite Doença de Chagas?',
+    answers: [
+      {
+        answer: 'Abelha',
+        correct: false
+      },
+      {
+        answer: 'Barata',
+        correct: false
+      },
+      {
+        answer: 'Pulga',
+        correct: false
+      },
+      {
+        answer: 'Barbeiro',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: 'Qual fruto é conhecido no Norte e Nordeste como "jerimum"?',
+    answers: [
+      {
+        answer: 'Caju',
+        correct: false
+      },
+      {
+        answer: 'Abóbora',
+        correct: true
+      },
+      {
+        answer: 'Chuchu',
+        correct: false
+      },
+      {
+        answer: 'Côco',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Qual é o coletivo de cães?',
+    answers: [
+      {
+        answer: 'Matilha',
+        correct: true
+      },
+      {
+        answer: 'Rebanho',
+        correct: false
+      },
+      {
+        answer: 'Alcateia',
+        correct: false
+      },
+      {
+        answer: 'Manada',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Qual é o triângulo que tem todos os lados diferentes?',
+    answers: [
+      {
+        answer: 'Equilátero',
+        correct: false
+      },
+      {
+        answer: 'Isóceles',
+        correct: false
+      },
+      {
+        answer: 'Escaleno',
+        correct: true
+      },
+      {
+        answer: 'Trapézio',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Quem compôs o Hino da Independência?',
+    answers: [
+      {
+        answer: 'Dom Pedro I.',
+        correct: true
+      },
+      {
+        answer: 'Manuel Bandeira',
+        correct: false
+      },
+      {
+        answer: 'Castro Alves',
+        correct: false
+      },
+      {
+        answer: 'Carlos Gomes',
+        correct: false
+      }
+    ]
+  }
+]
---
+++ projects/day-14-quiz/assets/js/main.js
@@ -0,0 +1,111 @@
+import { questions } from './data.js'
+
+console.log(questions)
+
+const scoreboard = document.querySelector('.scoreboard')
+const quizDiv = document.querySelector('.quiz')
+const question = document.querySelector('#question')
+const answersDiv = document.querySelector('.answers')
+
+const letters = ['a', 'b', 'c', 'd']
+
+let points = 0
+let currentQuestion = 0
+
+function initQuiz() {
+  createQuestion(0)
+}
+
+function createQuestion(i) {
+  const prevQuestion = answersDiv.querySelectorAll('button')
+  prevQuestion.forEach(function (btn) {
+    btn.remove()
+  })
+
+  const questionNum = question.querySelector('#question-id')
+  const questionText = question.querySelector('#question-txt')
+
+  questionNum.textContent = i + 1
+  questionText.textContent = questions[i].question
+
+  questions[i].answers.forEach(function (answer, i) {
+    const answTemplate = document
+      .querySelector('.answers-template')
+      .cloneNode(true)
+    const answLetter = answTemplate.querySelector('#answ-letter')
+    const answText = answTemplate.querySelector('#answ-text')
+
+    answLetter.textContent = letters[i]
+    answText.textContent = answer['answer']
+
+    answTemplate.setAttribute('correct-answ', answer['correct'])
+
+    answTemplate.classList.remove('hidden')
+    answTemplate.classList.remove('answers-template')
+
+    answersDiv.appendChild(answTemplate)
+
+    answTemplate.addEventListener('click', () => {
+      checkAnswer(this)
+    })
+  })
+  currentQuestion++
+}
+
+function checkAnswer(btn) {
+  const buttons = answersDiv.querySelectorAll('button')
+
+  buttons.forEach(function (button) {
+    if (button.getAttribute('correct-answ') === 'true') {
+      button.classList.add('correct-answ')
+      if (btn === button) {
+        points++
+      }
+    } else {
+      button.classList.add('wrong-answ')
+    }
+  })
+  nextQuestion()
+}
+
+function nextQuestion() {
+  setTimeout(() => {
+    if (currentQuestion >= questions.length) {
+      endGame()
+      return
+    }
+    createQuestion(currentQuestion)
+  }, 1250)
+}
+
+function endGame() {
+  showScore()
+  const score = ((points / questions.length) * 100).toFixed(0)
+
+  const displayScore = scoreboard.querySelector('scoreboard--display span')
+
+  displayScore.textContent = score.toString()
+
+  const correctAnsw = document.querySelector('#scoreboard-correct-answ')
+  correctAnsw.textContent = points
+
+  const totalQuestions = document.querySelector('#scoreboard--total-questions')
+  totalQuestions.textContent = questions.length
+
+  // To do message
+}
+
+function showScore() {
+  scoreboard.classList.toggle('hidden')
+  quizDiv.classList.toggle('hidden')
+}
+
+const restartQuiz = document.querySelector('#restart')
+restartQuiz.addEventListener('click', () => {
+  currentQuestion = 0
+  points = 0
+  showScore()
+  initQuiz()
+})
+
+initQuiz()
