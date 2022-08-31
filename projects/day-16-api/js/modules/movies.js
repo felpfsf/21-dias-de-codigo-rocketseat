afbec7eaca5d5b2d88171840b25a11c6a31899eb
felpfsf <feferreira.sf@gmail.com>
2022-08-31 18:17:39 -0300

    update: style and featured random movie poster

--- projects/day-16-api/js/modules/movies.js
+++ projects/day-16-api/js/modules/movies.js
@@ -2,24 +2,57 @@ import { getPopMovies } from './api.js'
 import { config } from './config.js'
 
 const moviesElem = document.querySelector('#movies')
-// console.log(moviesElem);
+const featElem = document.querySelector('#featured-movie')
 
 export async function render() {
   const movies = await getPopMovies()
-  console.log(movies)
+
+  const movie = movies[Math.floor(Math.random() * movies.length)]
+  console.log(movie)
 
   moviesElem.innerHTML = movies?.map(movie => renderMovie(movie)).join('')
+  featElem.innerHTML = renderFeat(movie)
 }
 
 export function renderMovie(movie) {
   return `
-      <div>
+    <div class="movie-card">
+
+      <div class="poster">
         <img src="${
           movie?.poster_path
             ? config.POSTER_URL + movie?.poster_path
             : config.PLACEHOLDER
         }">
-        <h2>${movie?.title}</h2>
       </div>
+
+      <div class="content">
+        <h2 class="movie-title">${movie.title}</h2>
+        <h3 class="movie-rate">
+          <span class="material-symbols-outlined">grade</span> ${
+            movie.vote_average
+          }</h3>
+      </div>
+
+    </div>
     `
+}
+
+export function renderFeat(movie) {
+  return `
+    <div class="featured-poster">
+      <div class="overlay"></div>
+      <img src="${
+        movie?.poster_path
+          ? config.POSTER_URL + movie?.poster_path
+          : config.PLACEHOLDER
+      }">
+    </div>
+    <div class="featured-content">
+      <h1 class="feat-movie-title">${movie.title}</h1>
+      <p class="feat-movie-overview">${movie.overview}</p>
+      <p class="feat-movie-release">Released: ${movie.release_date}</p>
+      
+    </div>
+  `
 }
