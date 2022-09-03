aef7e9b074bcf25e9c9289f342560b7a0d4d4aa2
felpfsf <feferreira.sf@gmail.com>
2022-09-03 13:21:45 -0300

    feat: random images from categories

---
+++ projects/day-19-unsplash-random/js/main.js
@@ -0,0 +1,22 @@
+const btnGenRandom = document.querySelectorAll('.btnGenImg')
+const imgContainer = document.querySelector('.image-container')
+
+const URL_RANDOM = 'https://source.unsplash.com/random/500x500/'
+
+for (let i = 0; i < btnGenRandom.length; i++) {
+  btnGenRandom[i].addEventListener('click', () => {
+    const attr = btnGenRandom[i].dataset.key
+    console.log(btnGenRandom[i].dataset.key)
+    renderImg(attr)
+  })
+}
+
+function renderImg(attr) {
+  fetch(`${URL_RANDOM}?${attr}`)
+    .then(response => {
+      const fetchedImage = response.url
+      console.log(fetchedImage);
+      imgContainer.innerHTML = `<img src="${fetchedImage}">`
+    })
+    .catch(err => console.log(`Error: ${err}`))
+}
