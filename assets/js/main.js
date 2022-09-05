d29282a9266110e533cea7dab3fa56ce19cc48bc
felpfsf <feferreira.sf@gmail.com>
2022-09-05 19:46:08 -0300

    feat: scroll to the top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 const navToggle = document.querySelector('.mobile-toggle')
 const navMenu = document.querySelector('.nav-menu')
+const navMenuLinks = document.querySelectorAll('.nav-menu a')
+const btnScrollToTheTop = document.querySelector('#btnScrollToTheTop')
+console.log(btnScrollToTheTop);
+
+window.addEventListener('scroll', scrollToTheTop)
+
+navMenuLinks.forEach(item => {
+  item.addEventListener('click', openMenu)
+})
 
 navToggle.addEventListener('click', openMenu)
 
@@ -13,5 +22,13 @@ function openMenu() {
     navMenu.setAttribute('data-visible', false)
     navToggle.setAttribute('aria-expanded', false)
     navToggle.innerHTML = '<i id="menu-icon" class="ph-list-light"></i>'
+  }
+}
+
+function scrollToTheTop() {
+  if (scrollY > 300) {
+    btnScrollToTheTop.classList.add('isVisible')
+  } else {
+    btnScrollToTheTop.classList.remove('isVisible')
   }
 }
